87f5df74c1ef4efade6d3ee7227a78a546088984
Manishaallanki <manisharepala97@gmail.com>
2025-07-29 16:09:41 -0700

    Update streamlit_app.py
    url changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if ingredients_list:
     for fruits_choosen in ingredients_list:
         ingredients_string += fruits_choosen + ' '
         st.subheader(fruits_choosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruits_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
