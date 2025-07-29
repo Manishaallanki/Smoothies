960075356a8704a4a877ce753823165e4b5c76ac
Manishaallanki <manisharepala97@gmail.com>
2025-07-29 15:42:47 -0700

    Update streamlit_app.py
    Request library imported

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
-
-
-
-
+import requests
+smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
